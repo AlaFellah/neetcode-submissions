fa99e0d20c542bb2f908558f112cc870592a0d75
Ala Fellah <62778226+AlaFellah@users.noreply.github.com>
2026-07-18 16:19:06 +0300

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+ 
+        for i in range(len(nums)):
+            s = target - nums[i]
+            if s in nums[i+1::]:
+                sec = nums[i+1::].index(s)
+                ind = [i, sec+i+1]
+                return ind
+        return []
+        
+
+        # prevdiff = {}
+        # for i in range (len(nums)):
+        #     if target - nums[i] in prevdiff:
+        #         return [prevdiff[target - nums[i]], i]
+
+        #     prevdiff[nums[i]] = i 
+
+        # return   
+
